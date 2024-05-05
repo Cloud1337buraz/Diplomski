b23d7f0757d9cebb22d05d8cf1607fb257e104fb
Cloud1337buraz <74748797+Cloud1337buraz@users.noreply.github.com>
2024-05-05 22:01:28 +0200

    Animations - Upgrade

--- Diplomski/Assets/Scripts/Movement.cs
+++ Diplomski/Assets/Scripts/Movement.cs
@@ -8,8 +8,6 @@ public class Movement : MonoBehaviour
     public CharacterController controller;
     public Animator CharacterAnimator;
 
-    bool jumpAnimation;
-
     public Transform groundCheck;
     public Transform ceilingCheck;
     public Transform PlayerModel;
@@ -25,7 +23,7 @@ public class Movement : MonoBehaviour
     public float crouchHeight;
 
     Vector3 velocity;
-    Vector3 normalHeight;
+    
     public bool isUnderSomething;
     public bool isGrounded;
     public bool isCrouching;
@@ -34,22 +32,34 @@ public class Movement : MonoBehaviour
     {
         currentSpeed = walkSpeed;
         crouchSpeed = walkSpeed / 2;
-        normalHeight = transform.localScale;
 
         CharacterAnimator = PlayerModel.GetComponent<Animator>();
         CharacterAnimator.SetBool("Jump", false);
         CharacterAnimator.SetBool("JumpLand", false);
+        CharacterAnimator.SetBool("RunForward", false);
+        CharacterAnimator.SetBool("RunBackward", false);
+        CharacterAnimator.SetBool("CrouchForward", false);
+        CharacterAnimator.SetBool("CrouchBackward", false);
+        CharacterAnimator.SetBool("CrouchIdle", false);
     }
     void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.red;
-        Vector3 direction = Vector3.up * ceilingDistance;
-        Gizmos.DrawRay(ceilingCheck.position, direction);
+        Gizmos.color = Color.green;
+        Vector3 direction = Vector3.down * groundDistance;
+        Gizmos.DrawRay(groundCheck.position, direction);
+    }
+    bool isPlaying(string stateName)
+    {
+        if (CharacterAnimator.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
+                CharacterAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+            return true;
+        else
+            return false;
     }
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        
+        isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, groundDistance, groundMask);
+
 
         if (isGrounded && velocity.y < 0)
         {
@@ -57,12 +67,44 @@ public class Movement : MonoBehaviour
         }
 
         float x = Input.GetAxis("Horizontal");
-
         Vector3 move = transform.right * x;
-
-        controller.Move(move * currentSpeed * Time.deltaTime);
-
-        if(Input.GetKeyDown("w") && isGrounded)
+        if(!(isPlaying("Jump - Land") || isPlaying("Stand To Crouch") || isPlaying("Crouch To Stand")))
+        {
+            controller.Move(move * currentSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D) && isGrounded && !isCrouching)
+        {
+            CharacterAnimator.SetBool("RunForward", true);
+        }
+        else
+        {
+            CharacterAnimator.SetBool("RunForward", false);
+        }
+        if(Input.GetKey(KeyCode.A) && isGrounded && !isCrouching)
+        {
+            CharacterAnimator.SetBool("RunBackward", true);
+        }
+        else
+        {
+            CharacterAnimator.SetBool("RunBackward", false);
+        }
+        if (Input.GetKey(KeyCode.D) && isGrounded && isCrouching)
+        {
+            CharacterAnimator.SetBool("CrouchForward", true);
+        }
+        else
+        {
+            CharacterAnimator.SetBool("CrouchForward", false);
+        }
+        if (Input.GetKey(KeyCode.A) && isGrounded && isCrouching)
+        {
+            CharacterAnimator.SetBool("CrouchBackward", true);
+        }
+        else
+        {
+            CharacterAnimator.SetBool("CrouchBackward", false);
+        }
+        if (Input.GetKeyDown("w") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
             isCrouching = false;
@@ -76,6 +118,11 @@ public class Movement : MonoBehaviour
         {
             CharacterAnimator.SetBool("Jump", true);
             CharacterAnimator.SetBool("JumpLand", false);
+            CharacterAnimator.SetBool("CrouchIdle", false);
+            CharacterAnimator.SetBool("RunForward", false);
+            CharacterAnimator.SetBool("RunBackward", false);
+            CharacterAnimator.SetBool("CrouchBackward", false);
+            CharacterAnimator.SetBool("CrouchForward", false);
         }
         if (Input.GetKey(KeyCode.S) && isGrounded)
         {
@@ -108,16 +155,14 @@ public class Movement : MonoBehaviour
         {
             isCrouching = true;
         }
-        if (isCrouching)
+        if (isCrouching && isGrounded)
         {
-            if(transform.localScale.y != crouchHeight)
-            transform.localScale = new Vector3(normalHeight.x, crouchHeight, normalHeight.z);
+            CharacterAnimator.SetBool("CrouchIdle", true);
             currentSpeed = crouchSpeed;
         }
-        if(!isCrouching)
+        if(!isCrouching && isGrounded)
         {
-            if(transform.localScale.y != normalHeight.y)
-            transform.localScale = normalHeight;
+            CharacterAnimator.SetBool("CrouchIdle", false);
             currentSpeed = walkSpeed;
         }
         velocity.y += gravity * Time.deltaTime;
