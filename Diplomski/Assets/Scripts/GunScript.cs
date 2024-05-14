5d31e0a329376189b5bfe82d15af7896a0796dbf
Cloud1337buraz <74748797+Cloud1337buraz@users.noreply.github.com>
2024-05-14 23:04:31 +0200

    Gun fire rate, muzzle flash and sound added

--- Diplomski/Assets/Scripts/GunScript.cs
+++ Diplomski/Assets/Scripts/GunScript.cs
@@ -5,32 +5,63 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
+    [Header("Assignables")]
     public LayerMask layerMask;
+    public GameObject muzzleFlash;
+    public Transform muzzleFlashPosition;
 
+    public AudioClip gunShot;
+    private AudioSource audioS;
 
+    [Header("Stats")]
+    public float fireRate = 0.5f;
+
+    public float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 0;
+
+        audioS = gameObject.AddComponent<AudioSource>();
+        audioS.playOnAwake = false;
+        audioS.clip = gunShot;
+        audioS.Stop();
     }
 
+    public void Shoot()
+    {
+        GameObject Light = Instantiate(muzzleFlash, muzzleFlashPosition);
+        Destroy(Light, 0.1f);
+
+        GameObject Flash = Instantiate(muzzleFlash, muzzleFlashPosition);
+        Destroy(Flash, 0.1f);
+    }
     // Update is called once per frame
     void Update()
     {
+        time += Time.deltaTime;
+        float nextTimeToFire = fireRate / 1000;
+
         RaycastHit hit;
         Vector3 dir = transform.TransformDirection(Vector3.forward);
         Vector3 dir2 = new Vector3(dir.x, dir.y, 0);
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (Physics.Raycast(transform.position, Vector3.Normalize(dir2), out hit, Mathf.Infinity, layerMask))
+            if(time >= nextTimeToFire)
             {
-                hit.transform.gameObject.GetComponent<EnemyHitScript>().Damage(25);
-                Debug.Log(hit.transform.gameObject.GetComponent<EnemyHitScript>().health);
-            }
-            else
-            {
-                Debug.Log("Did not Hit");
+                Shoot();
+                audioS.Play();
+                if (Physics.Raycast(transform.position, Vector3.Normalize(dir2), out hit, Mathf.Infinity, layerMask))
+                {
+                    hit.transform.gameObject.GetComponent<EnemyHitScript>().Damage(25);
+                    Debug.Log(hit.transform.gameObject.GetComponent<EnemyHitScript>().health);
+                }
+                else
+                {
+                    Debug.Log("Did not Hit");
+                }
+                time = 0;
             }
         }
         Debug.DrawRay(transform.position, Vector3.Normalize(dir2) * 1000, Color.red);
